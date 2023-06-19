279c33bdd568d8fd965279ee26341b86e589d71d
unknown <joelapuk11@gmail.com>
2023-06-19 20:16:48 +0700

    website_1.0

---
+++ NW_Website/Prof/meals.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1 style="color: lightgreen;">Meals</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="meals.php">Home</a></li>
+          <li class="breadcrumb-item active">Meals</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      <div class="row">
+    
+        <!-- Left side columns -->
+        <div class="col-lg-8">
+          <div class="row">
+           
+    
+            
+          
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <!---<footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>NutriWise</span></strong>. All Rights Reserved
+    </div>
+
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
