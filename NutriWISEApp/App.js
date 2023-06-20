2f884c8f2cc9ac2b883510efaf95d3da78ba76b6
jephlandicho <landichojjl@gmail.com>
2023-06-20 13:38:43 +0800

    try lang

--- NutriWISEApp/App.js
+++ NutriWISEApp/App.js
@@ -1,22 +1,45 @@
-import React from 'react';
-import { View,SafeAreaView, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, SafeAreaView, StyleSheet, Alert } from 'react-native';
+import * as SQLite from 'expo-sqlite';
 
 import Navigation from './src/navigation';
 
-const App =() => {
-  return(
-    <SafeAreaView style={styles.root}>
-    <Navigation />
-  </SafeAreaView>
-  );
+const db = SQLite.openDatabase('nw.db');
 
-}
+const App = () => {
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT 1 FROM sqlite_master WHERE type="table" AND name="meals"',
+        [],
+        () => {
+          Alert.alert('Success', 'SQLite database connected successfully!');
+        },
+        (txObj, error) => {
+          console.log('Database error:', error);
+          Alert.alert('Error', 'Failed to connect to SQLite database: ' + error.message);
+        }
+      );
+    });
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <View style={styles.container}>
+        <Navigation />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  root:{
+  root: {
     flex: 1,
-    backgroundColor: '#F9FBFC'
-  }
-})
+    backgroundColor: '#F9FBFC',
+  },
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
--- NutriWISEApp/src/screens/SettingsScreen/Settings.js
+++ NutriWISEApp/src/screens/SettingsScreen/Settings.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('nw.db');
 
 const Settings = () => {
+  const [mealNames, setMealNames] = useState([]);
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='meals'",
+        [],
+        (_, { rows }) => {
+          if (rows.length > 0) {
+            db.transaction(tx => {
+              tx.executeSql(
+                'SELECT meal_name FROM meals',
+                [],
+                (_, { rows }) => {
+                  if (rows.length > 0) {
+                    const names = rows._array.map(item => item.meal_name);
+                    setMealNames(names);
+                  }
+                },
+                (_, error) => {
+                  console.error('Error fetching meal names:', error);
+                }
+              );
+            });
+          }
+        },
+        (_, error) => {
+          console.error('Error checking for meals table:', error);
+        }
+      );
+    });
+  }, []);
+
   return (
     <View>
-      <Text>Settings</Text>
+      {mealNames.map((mealName, index) => (
+        <Text key={index}>{mealName}</Text>
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default Settings
+export default Settings;
