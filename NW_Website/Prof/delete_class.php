fc2af8fd9170a9511c68ca89adbc6f45eef20e2d
unknown <joelapuk11@gmail.com>
2023-08-14 11:36:50 +0700

    website_2.6

--- NW_Website/Prof/delete_class.php
+++ NW_Website/Prof/delete_class.php
@@ -8,12 +8,12 @@ if (isset($_POST['class_id'])) {
     $classId = $_POST['class_id'];
 
     try {
-        // Update the associated schedule records before deleting the class
-        $queryUpdateSchedule = "UPDATE class_schedule SET class_id = NULL WHERE class_id = ?";
-        $stmtUpdateSchedule = mysqli_prepare($conn, $queryUpdateSchedule);
-        mysqli_stmt_bind_param($stmtUpdateSchedule, "i", $classId);
-        mysqli_stmt_execute($stmtUpdateSchedule);
-        mysqli_stmt_close($stmtUpdateSchedule);
+        // Delete associated class schedules
+        $queryDeleteSchedules = "DELETE FROM class_schedule WHERE class_id = ?";
+        $stmtDeleteSchedules = mysqli_prepare($conn, $queryDeleteSchedules);
+        mysqli_stmt_bind_param($stmtDeleteSchedules, "i", $classId);
+        $resultDeleteSchedules = mysqli_stmt_execute($stmtDeleteSchedules);
+        mysqli_stmt_close($stmtDeleteSchedules);
 
         // Delete the class from the classes table
         $queryDeleteClass = "DELETE FROM classes WHERE id = ?";
@@ -22,8 +22,8 @@ if (isset($_POST['class_id'])) {
         $resultDeleteClass = mysqli_stmt_execute($stmtDeleteClass);
 
         if ($resultDeleteClass) {
-            // Class deleted successfully
-            echo '<script>alert("Class successfully deleted.");</script>';
+            // Class and associated class schedules deleted successfully
+            echo '<script>alert("Class and associated class schedules successfully deleted.");</script>';
             echo '<script>window.location.href = "classes.php";</script>';
         } else {
             // Failed to delete class, handle the error
