52b5ec4922aae3146e44ac4f12f122894685484a
jephlandicho <landichojjl@gmail.com>
2023-09-26 13:00:31 +0800

    Client Application

---
+++ NW_Client/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SignInScreen from './src/Auth/SignInScreen';
+import Navigation from './src/Navigation/Navigation';
+import SyncComponent from './src/Components/SyncComponent';
+
+export const ResultContext = React.createContext();
+
+const App = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  React.useEffect(() => {
+    const checkUserLoginStatus = async () => {
+      try {
+        const userAccountString = await AsyncStorage.getItem('clientDataaaa');
+        if (userAccountString) {
+          const userAccount = JSON.parse(userAccountString);
+          // Perform any additional checks or validations if needed
+
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        // Handle error while retrieving data
+        console.log('Error retrieving user account:', error);
+      }
+    };
+
+    checkUserLoginStatus();
+  }, []);
+
+  return (
+    <ResultContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      <View style={styles.container}>
+        {isLoggedIn ? (
+          <Navigation isLoggedIn={isLoggedIn} />
+        ) : (
+          <SignInScreen setLoggedIn={setLoggedIn} />
+        )}
+      </View>
+    </ResultContext.Provider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+export default App;
---
+++ NW_Client/src/BottomTab/Home.js
@@ -0,0 +1,193 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, ScrollView, FlatList } from 'react-native';
+import { Avatar, Card, Provider as PaperProvider } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import MyTheme from '../Components/MyTheme';
+import * as SQLite from 'expo-sqlite';
+const db = SQLite.openDatabase('mydatabase.db');
+
+const Home = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    try {
+      const userData = await AsyncStorage.getItem('clientDataaaa');
+      if (userData) {
+        const parsedUserData = JSON.parse(userData);
+        setUserData(parsedUserData);
+      } else {
+        // User data doesn't exist in local storage
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+
+  const otherData = [
+    {
+      title: 'Nutritional Status',
+      description: userData ? userData.remarks : '',
+    },
+    {
+      title: 'Body Mass Index',
+      description: userData ? userData.BMI + ' kg/m²' : '',
+    }
+  ];
+  const otherData1 = [
+    {
+      title: 'Weight',
+      description: userData ? userData.weight + ' kg' : '',
+    },
+    {
+      title: 'Height',
+      description: userData ? userData.height + ' m' : '',
+    },
+  ];
+
+  return (
+    <PaperProvider theme={MyTheme}>
+      <View style={styles.container}>
+        <ScrollView>
+          {userData && (
+            <View style={styles.header}>
+              <Avatar.Text size={64} label={userData.name.charAt(0).toUpperCase()} />
+              <Text style={styles.userName}>{userData.name}</Text>
+            </View>
+          )}
+        <Card style={styles.dietPresCard}>
+            <View style={styles.cardContent}>
+            <Text style={styles.cardTitle}>Diet Prescription</Text>
+              <Text style={styles.cardText}>{userData ? userData.TER : ''} kcal</Text>
+                <View style={styles.nutrientRow}>
+                  <View style={styles.nutrientColumn}>
+                    <Text style={styles.nutrientLabel}>Carbohydrates</Text>
+                    <Text style={styles.nutrientValue}>
+                      {userData ? userData.carbs : ''} g
+                    </Text>
+                  </View>
+                  <View style={styles.nutrientColumn}>
+                    <Text style={styles.nutrientLabel}>Protein</Text>
+                    <Text style={styles.nutrientValue}>
+                      {userData ? userData.protein : ''} g
+                    </Text>
+                  </View>
+                  <View style={styles.nutrientColumn}>
+                    <Text style={styles.nutrientLabel}>Fats</Text>
+                    <Text style={styles.nutrientValue}>
+                      {userData ? userData.fats : ''} g
+                    </Text>
+                  </View>
+                </View>
+            </View>
+          </Card>
+          {/* Render the other cards */}
+          <View style={styles.cardsRow}>
+            {otherData.map((item) => (
+              <Card key={item.title} style={styles.card}>
+                <View style={styles.cardContent}>
+                  <Text style={styles.cardTitle1}>{item.title}</Text>
+                  <Text style={styles.cardText}>{item.description}</Text>
+                </View>
+              </Card>
+            ))}
+            </View>
+            <View style={styles.cardsRow}>
+            {otherData1.map((item) => (
+              <Card key={item.title} style={styles.card}>
+                <View style={styles.cardContent}>
+                  <Text style={styles.cardTitle1}>{item.title}</Text>
+                  <Text style={styles.cardText}>{item.description}</Text>
+                </View>
+              </Card>
+            ))}
+            </View>
+          {/* Add more cards or components for the to-do list, announcements, attendance, resources, etc. */}
+        </ScrollView>
+      </View>
+    </PaperProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff', // Background color for the entire screen
+    paddingTop: 30,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 16,
+  },
+  userName: {
+    marginLeft: 16,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333', // User name text color
+  },
+  dietPresCard: {
+    margin: 10,
+    borderRadius: 30,
+    elevation: 4,
+    backgroundColor: '#abc88f', // Card background color for "Diet Prescription"
+    width: 370,
+    height: 200,
+  },
+  card: {
+    margin: 10,
+    borderRadius: 10, // Increase card border radius for a modern look
+    elevation: 4,
+    backgroundColor: '#fff', // Card background color
+    width: 175, // Set the width of each card as needed
+  },
+  cardContent: {
+    alignItems: 'center', // Center the content horizontally
+    padding: 15,
+  },
+  cardTitle1:{
+    textAlign: 'center', // Center the text horizontally
+    fontSize: 18, // Increase font size for better readability
+    color: '#000000', // Text color
+    marginBottom: 10,
+  },
+  cardTitle:{
+    fontSize: 25, // Increase font size for better readability
+    color: '#ffffff', // Text color
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  cardText: {
+    textAlign: 'center', // Center the text horizontally
+    fontSize: 24, // Increase font size for better readability
+    color: '#004e04',  // Text color
+    fontWeight: 'bold',
+  },
+  nutrientRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 50,
+  },
+  nutrientColumn: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  nutrientLabel: {
+    fontSize: 16,
+    color: '#ffffff',
+  },
+  nutrientValue: {
+    fontSize: 18,
+    color: '#000000',
+  },
+  cardsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
+export default Home;
