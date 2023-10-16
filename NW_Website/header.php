dc724752478d31c387960d945fc6899d8e644b5d
unknown <joelapuk11@gmail.com>
2023-10-16 10:20:39 +0700

    New branch

--- NW_Website/header.php
+++ NW_Website/header.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
+   <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NutriWise</title>
 
-    <!-- Loading third party fonts -->
+    <!-- Loading third-party fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
     <link href="fonts/font-awesome.min.css" rel="stylesheet">
     <link href="fonts/iconmoon.css" rel="stylesheet">
@@ -15,7 +13,9 @@
     <!-- Loading main CSS file -->
     <link rel="stylesheet" href="style.css">
     <style>
-        table {
+        /* Existing CSS styles */
+
+           table {
             width: 100%;
             border-collapse: collapse;
         }
@@ -48,9 +48,206 @@
             max-height: 40px;
             margin-right: 10px;
         }
+
+        .menu {
+            list-style: none;
+            margin: 20px 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+        }
+
+        .menu-item {
+            margin-right: 10px;
+            text-decoration: none;
+            color: orange;
+        }
+
+        .separator {
+            margin-right: 10px;
+            margin-left: 10px;
+            color: orange;
+        }
+
+        .menu-item:hover {
+            color: #007bff;
+        }
+
+        @media screen and (max-width: 768px) {
+            .menu {
+                display: none;
+                flex-direction: column;
+                background-color: #fff;
+                position: absolute;
+                top: 60px;
+                left: 0;
+                right: 0;
+                z-index: 1;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            }
+
+            .menu.active {
+                display: flex;
+            }
+
+            .menu-item {
+                margin: 10px 0;
+                margin-right: 0;
+            }
+        }
+
+
+        /* Modal Styles */
+        .modal {
+            display: none;
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.7);
+        }
+
+        .modal-content {
+            background-color: #fff;
+            margin: 15% auto;
+            padding: 20px;
+            border-radius: 5px;
+            text-align: center;
+            width: 50%;
+            position: relative;
+        }
+
+        .close {
+            position: absolute;
+            top: 10px;
+            right: 15px;
+            font-size: 24px;
+            cursor: pointer;
+        }
+        .modal {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    overflow: auto;
+}
+
+.modal-content {
+    background-color: #fff;
+    margin: 10% auto;
+    padding: 20px;
+    border-radius: 5px;
+    text-align: center;
+    max-width: 400px;
+    position: relative;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    animation-name: modalopen;
+    animation-duration: 0.4s;
+}
+
+.close {
+    position: absolute;
+    top: 5px;
+    right: 10px;
+    font-size: 24px;
+    cursor: pointer;
+    color: #888;
+}
+
+h2 {
+    color: #333;
+}
+
+button {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    margin: 10px;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
+
+/* Responsive Modal Styles */
+@media screen and (max-width: 768px) {
+    .modal-content {
+        margin: 15% auto;
+        max-width: 90%;
+    }
+}
+
+@keyframes modalopen {
+    from {opacity: 0}
+    to {opacity: 1}
+}
+.menu-toggle {
+    display: none;
+    background: none;
+    border: none;
+    cursor: pointer;
+    padding: 5px;
+    margin-right: 10px;
+    font-size: 24px;
+}
+
+.menu-toggle .bar {
+    display: block;
+    width: 25px;
+    height: 3px;
+    margin: 5px auto;
+    background-color: #333;
+    transition: 0.4s;
+}
+
+.menu.active .menu-toggle .bar:nth-child(1) {
+    transform: rotate(-45deg) translate(-5px, 6px);
+}
+
+.menu.active .menu-toggle .bar:nth-child(2) {
+    opacity: 0;
+}
+
+.menu.active .menu-toggle .bar:nth-child(3) {
+    transform: rotate(45deg) translate(-5px, -6px);
+}
+
+.menu {
+    list-style: none;
+    margin: 20px 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.menu-item {
+    text-decoration: none;
+    color: #333;
+    font-weight: 600;
+    padding: 10px 15px;
+    margin: 0 10px;
+    border-radius: 5px;
+    transition: background-color 0.3s, color 0.3s;
+}
+
+.menu-item:hover {
+    background-color: #7cff7c; /* Light green background color */
+    color: #333; /* Text color when hovered */
+}
+
     </style>
 </head>
-
 <body class="homepage">
     <header class="site-header">
         <div class="container">
@@ -60,19 +257,75 @@
                     <h1>NutriWise</h1>
                 </a>
             </div>
-            <!-- Default snippet for navigation -->
-            <div class="main-navigation">
-                <button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
-                <ul class="menu">
-                    <li class="menu-item"><a href="index.php">Home</a></li>
-                    <li class="menu-item"><a href="about.php">About Us</a></li>
-                    <li class="menu-item"><a href="sample.php">Sample Meal Plan</a></li>
-                    <li class="menu-item"><a href="app.php">Download Our App</a></li>
-                    <li class="menu-item"><a href="Prof/login/login_form.php">Log In</a></li>
-                </ul>
+            <!-- Mobile menu toggle -->
+            <button type="button" class="menu-toggle" id="mobile-menu-toggle">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </button>
+            <div class="menu" id="menu">
+                <a class="menu-item" href="index.php">Home</a>
+                <a class="menu-item" href="about.php">About Us</a>
+                <a class="menu-item" href="sample.php">Articles </a>
+                <a id="downloadAppLink" class="menu-item" href="#">Download Our App</a>
+                <a class="menu-item" href="Prof/login/login_form.php">Log In</a>
             </div>
         </div>
     </header>
+
+  <!-- Modal -->
+<div id="downloadModal" class="modal">
+    <div class="modal-content">
+        <span class="close" id="closeModal">&times;</span>
+        <h2>Download Our App</h2>
+        <p>Choose your preferred app:</p>
+        <button id="clientAppBtn">Client App</button>
+        <button id="mobileAppBtn">Mobile App</button>
+    </div>
+</div>
+
+    <script>
+        // JavaScript to toggle the mobile menu
+        document.addEventListener('DOMContentLoaded', function () {
+            var mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+            var menu = document.getElementById('menu');
+            var modal = document.getElementById('downloadModal');
+            var closeModal = document.getElementById('closeModal');
+            var downloadAppLink = document.getElementById('downloadAppLink');
+            var clientAppBtn = document.getElementById('clientAppBtn');
+            var mobileAppBtn = document.getElementById('mobileAppBtn');
+
+            mobileMenuToggle.addEventListener('click', function () {
+                menu.classList.toggle('active');
+            });
+
+            // Open the modal when "Download Our App" link is clicked
+            downloadAppLink.addEventListener('click', function (e) {
+                e.preventDefault();
+                modal.style.display = 'block';
+            });
+
+            // Close the modal when the close button is clicked
+            closeModal.addEventListener('click', function () {
+                modal.style.display = 'none';
+            });
+
+            // Handle button clicks
+            clientAppBtn.addEventListener('click', function () {
+                // Replace this with your client app download logic
+                alert('Redirect to the client app download page');
+                modal.style.display = 'none'; // Close the modal after the action
+            });
+
+            mobileAppBtn.addEventListener('click', function () {
+                // Replace this with your mobile app download logic
+                alert('Redirect to the mobile app download page');
+                modal.style.display = 'none'; // Close the modal after the action
+            });
+        });
+    </script>
+     
+
 </body>
 
 </html>
--- NW_Website/sample.php
+++ NW_Website/sample.php
@@ -1,101 +1,254 @@
-<?php include 'header.php'
-?>			<main class="main-content">
-				
-				<div class="content">
-					<div class="container">
-						
-						<h2 class="entry-title">This is an Example of Meal Plan</h2>
-						<p>
+<?php include 'header.php'; ?>
 
-							The purpose of a sample meal plan is to promote a balanced diet that includes all the necessary macronutrients and micronutrients for optimal health. Macronutrients, such as carbohydrates, proteins, and fats, provide energy and support various bodily functions. Micronutrients, including vitamins and minerals, are essential for proper functioning of the body's systems and maintaining overall well-being.
- 						</p>
+<main class="main-content">
+    <div class="content">
+        <div class="container">
+            <div class="row" id="news-container">
+                <!-- Nutrition news articles will be displayed here as cards -->
+            </div>
+            <!-- <div class="row" id="load-more-container">
+                <div class="col-md-12 text-center">
+                    <button id="load-more-button" class="btn btn-primary">See More</button>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</main>
+<footer class="site-footer">
+    <div class="container">
+        <nav class="footer-navigation">
+            <a href="#">About US</a>
+            <a href="#">Offer</a>
+            <a href="#">Articles</a>
+            <a href="#">Contact</a>
+        </nav>
 
-						<div class="recipes-list">
-							<article class="recipe">
-								<figure class="recipe-image"><img src="images/plan1.png" alt="Food 1"></figure>
-								<div class="recipe-detail">
-									<h2 class="recipe-title"><a href="single.php">Example meal plan for Underweight</a></h2>
-									<p>Our underweight meal plan is a carefully curated eating regimen designed to help individuals gain healthy weight. Packed with calorie-dense foods and nutrient-rich ingredients, it provides a balanced combination of carbohydrates, proteins, and healthy fats to support your body's growth and overall health. With our expertly crafted meals, you can achieve your weight goals while nourishing your body with essential nutrients.</p>
-									<div class="recipe-meta">
-										
-										
-										
-									</div>
-								</div>
-							</article>
-							<article class="recipe">
-								<figure class="recipe-image"><img src="images/plan1.png" alt="Food 2"></figure>
-								<div class="recipe-detail">
-									<h2 class="recipe-title"><a href="single2.php">Example meal plan for Healthy Weight</a></h2>
-									<p>	Our normal weight meal plan is thoughtfully designed to support a healthy weight maintenance. It offers a well-balanced selection of nutritious foods, ensuring you receive an optimal combination of carbohydrates, proteins, and healthy fats. With a focus on whole, unprocessed ingredients, our meals provide essential nutrients to fuel your body and promote overall well-being. Whether you're looking to maintain your current weight or adopt a healthier lifestyle, our carefully crafted meal plan can help you achieve your goals while nourishing your body with wholesome goodness</p>
-									<div class="recipe-meta">
-										
-										
-										
-									</div>
-								</div>
-							</article>
-							<article class="recipe">
-								<figure class="recipe-image"><img src="images/plan1.png" alt="Food 3"></figure>
-								<div class="recipe-detail">
-									<h2 class="recipe-title"><a href="single3.html">Example meal plan for Overweight</a></h2>
-									<p>Our overweight meal plan is a thoughtfully designed eating regimen tailored to help individuals achieve a healthy weight. It focuses on balanced meals that prioritize nutrient-dense foods and portion control. By incorporating a variety of wholesome ingredients, our meal plan aims to support your weight management goals while ensuring your body receives essential nutrients. With the guidance of our expertly crafted meals, you can work towards a healthier weight while nourishing your body."</p>
-									<div class="recipe-meta">
-										 
-										
-										
-									</div>
-								</div>
-							</article>
-							<article class="recipe">
-								<figure class="recipe-image"><img src="images/plan1.png" alt="Food 4"></figure>
-								<div class="recipe-detail">
-									<h2 class="recipe-title"><a href="single4.html">Example meal for Obese</a></h2>
-									<p>Our obese weight meal plan is a thoughtfully designed eating program aimed at helping individuals achieve a healthy weight. It focuses on nutritious, low-calorie foods and portion control to promote weight loss and improve overall well-being. With a balance of wholesome ingredients, including lean proteins, whole grains, and plenty of fruits and vegetables, our meal plan supports healthy eating habits and gradual weight reduction. By following our carefully crafted meals, you can work towards your weight goals while nourishing your body with essential nutrients</p>
-									<div class="recipe-meta">
-										
-										
-									
-									</div>
-								</div>
-							</article>
-					
-						</div>
-					</div>
-				</div>
+        <div class="social-links">
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+            <a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a>
+        </div>
+        <div class="colophon">
+            <p>Copyright 2023 NutriWise. <br> All rights reserved</p>
+        </div>
+    </div>
+</footer>
 
-			</main> <!-- .main-content -->
-			
-			<footer class="site-footer">
-				<div class="container">
-					
-					<nav class="footer-navigation">
-						<a href="#">About US</a>
-						<a href="#">Offer</a>
-						<a href="#">Sample Meal Plan</a>
-						<a href="#">Contact</a>
-					</nav>
-					
-					<div class="social-links">
-						<a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-						<a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-						<a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-						<a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a>
-					</div>
-					<div class="colophon">
-						<p>Copyright 2023 NutriWise. <br> Alright reserved</p>
-					</div>
-				</div>
-			</footer>
+<!-- Add this CSS to your existing CSS file or create a new one -->
+<style>
+    .card {
+        background-color: #fff;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        margin-bottom: 20px;
+        padding: 10px; /* Adjusted padding for spacing */
+        max-width: 400px; /* Adjusted maximum width */
+    }
 
-		</div>
+    .card img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 8px 8px 0 0;
+    }
 
-		
+    .card h2 {
+        font-size: 1.25rem; /* Adjusted font size for titles */
+        margin-top: 10px;
+    }
 
-		<script src="js/jquery-1.11.1.min.js"></script>
-		<script src="js/plugins.js"></script>
-		<script src="js/app.js"></script>
-		
-	</body>
+    .card p {
+        font-size: 1rem; /* Adjusted font size for descriptions */
+    }
+
+    .card a {
+        color: #007BFF;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    #load-more-button {
+        margin-top: 20px;
+    }
+
+    /* Flexbox layout for news cards */
+    #news-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .card {
+        flex: 0 1 calc(33.33% - 20px); /* Three columns with spacing */
+    }
+
+    /* Responsive layout for smaller screens */
+    @media (max-width: 768px) {
+        #news-container {
+            flex-wrap: wrap;
+            justify-content: center; /* Center cards on smaller screens */
+        }
+
+        .card {
+            flex: 0 1 calc(50% - 20px); /* Two columns with spacing on smaller screens */
+        }
+    }
+
+    @media (max-width: 576px) {
+        .card {
+            flex: 0 1 100%; /* Single column on the smallest screens */
+        }
+    }
+</style><style>
+    .card {
+        background-color: #fff;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        margin-bottom: 20px;
+        padding: 10px; /* Adjusted padding for spacing */
+        max-width: 400px; /* Adjusted maximum width */
+    }
+
+    .card img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 8px 8px 0 0;
+    }
+
+    .card h2 {
+        font-size: 1.25rem; /* Adjusted font size for titles */
+        margin-top: 10px;
+    }
+
+    .card p {
+        font-size: 1rem; /* Adjusted font size for descriptions */
+    }
+
+    .card a {
+        color: #007BFF;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    #load-more-button {
+        margin-top: 20px;
+    }
+
+    /* Flexbox layout for news cards */
+    #news-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .card {
+        flex: 0 1 calc(33.33% - 20px); /* Three columns with spacing */
+    }
+
+    /* Responsive layout for smaller screens */
+    @media (max-width: 768px) {
+        #news-container {
+            flex-wrap: wrap;
+            justify-content: center; /* Center cards on smaller screens */
+        }
+
+        .card {
+            flex: 0 1 calc(50% - 20px); /* Two columns with spacing on smaller screens */
+        }
+    }
+
+    @media (max-width: 576px) {
+        .card {
+            flex: 0 1 100%; /* Single column on the smallest screens */
+        }
+    }
+</style>
+
+<script>
+    const apiKey = '40853a0b612a4a46b20cb1d938238e1e'; // Replace with your actual API key
+    const apiUrl = 'https://newsapi.org/v2/everything';
+    const keyword = 'nutrition';
+    const articlesPerPage = 12;
+    let currentPage = 1;
+	let totalArticles = 0; // Variable to store the total number of articles
+
+
+    // Function to create a news card
+    function createNewsCard(article) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const image = document.createElement('img');
+        image.src = article.urlToImage || 'https://via.placeholder.com/150'; // Use a placeholder image if no image is available
+        image.alt = article.title || 'Image Alt Text'; // Use fallback text if no title is available
+        card.appendChild(image);
+
+        const title = document.createElement('h2');
+        title.textContent = article.title || 'Title Not Available'; // Use fallback text if no title is available
+        card.appendChild(title);
+
+        const description = document.createElement('p');
+        description.textContent = article.description || 'Description Not Available'; // Use fallback text if no description is available
+        card.appendChild(description);
+
+        const link = document.createElement('a');
+        link.href = article.url || '#'; // Use a placeholder link if no URL is available
+        link.textContent = 'Read more';
+        card.appendChild(link);
+
+        return card;
+    }
+
+    // Function to fetch and display nutrition news articles
+	function fetchNutritionNews(page) {
+    const pageSize = articlesPerPage;
+    fetch(`${apiUrl}?q=${keyword}&apiKey=${apiKey}&pageSize=${pageSize}&page=${page}`)
+        .then(response => response.json())
+        .then(data => {
+            const articles = data.articles;
+            const newsContainer = document.getElementById('news-container');
+
+            if (page === 1) {
+                newsContainer.innerHTML = '';
+                totalArticles = data.totalResults; // Update totalArticles with the total number of articles
+            }
+
+            articles.slice((page - 1) * articlesPerPage, page * articlesPerPage).forEach(article => {
+                const newsCard = createNewsCard(article);
+                newsContainer.appendChild(newsCard);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching news:', error);
+        });
+}
+
+    // Function to load more news articles
+	function loadMoreArticles() {
+    if (currentPage * articlesPerPage < totalArticles) {
+        currentPage++;
+        fetchNutritionNews(currentPage);
+    } else {
+        // Disable the button or provide a message indicating no more articles to load
+        const loadMoreButton = document.getElementById('load-more-button');
+        loadMoreButton.disabled = true;
+        loadMoreButton.textContent = 'No More Articles';
+    }
+}
+
+    // Call the function to initially fetch and display the first 5 news articles
+    fetchNutritionNews(currentPage);
+
+    // Add an event listener to the "See More" button
+    const loadMoreButton = document.getElementById('load-more-button');
+    loadMoreButton.addEventListener('click', loadMoreArticles);
+</script>
+
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/app.js"></script>
+
+</body>
 
 </html>
+
